b5105c88005a2166a1cf1ffc0e1c8455cf361ad9
Ezequiel Leonardo Castaño <elcg@gmx.com>
2020-08-19 18:48:53 +0200

    Add new word Form

--- src/components/Menu/MenuFooter/MenuFooter.vue
+++ src/components/Menu/MenuFooter/MenuFooter.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="menu-footer">
+    <h3>
+      ¿Te gustaría agregar una palabra nueva? Envía tu sugerencia con <a href="https://forms.gle/2QtxSTZHAfjTtHqR7">este formulario</a>
+    </h3>
+    <br>
+
     Juego adaptado por <a href="http://elc.github.io">Ezequiel Castaño</a> | Desarrollado originalmente por <a href="https://kovah.de/">Kovah.de</a>
   </div>
 </template>
