937aa76398bd5cb14f66026ce246bc826a6ed52d
Ezequiel Leonardo Castaño <elcg@gmx.com>
2020-08-19 17:43:08 +0200

    Add Mixed Category

--- src/components/GameData.vue
+++ src/components/GameData.vue
@@ -3,21 +3,21 @@
 
   export default {
     name: 'GameData',
-    categories: TabooData.categories(),
+    categories: TabooData.categories('es'),
     colors: [
       {id: 'blue', value: '#1e88e5'},
       // {id: 'indigo', value: '#5e35b1'},
       // {id: 'purple', value: '#8e24aa'},
       // {id: 'pink', value: '#d81b60'},
       // {id: 'red', value: '#e53935'},
-      // {id: 'orange', value: '#fb8c00'},
+      {id: 'orange', value: '#fb8c00'},
       // {id: 'yellow', value: '#fdd835'},
       {id: 'green', value: '#43a047'},
       {id: 'teal', value: '#00acc1'},
       {id: 'cyan', value: '#039be5'}
     ],
     getCategories () {
-      return this.categories.es;
+      return this.categories;
     },
     getCardsForCategory (category) {
       return TabooData.getCategory(category, 'es');
--- src/data/data.js
+++ src/data/data.js
@@ -1,43 +1,23 @@
 const categories = require('./data/categories.json');
 
-const data = {
- classic: {},
- biblical: {},
-};
-
 const languages = ['de', 'es']
 
-languages.forEach(lang => {
-    data.classic[lang] = require('./data/' + lang + '/classic.json');
-    data.biblical[lang] = require('./data/' + lang + '/biblical.json');
-});
+let data = {};
 
+languages.forEach(language => {
+  data[language] = {};
+  Reflect.ownKeys(categories[language]).forEach(category =>  {
+    data[language][category] = require(`./data/${language}/${category}.json`);
+  })
+});
 
 export default class TabooData {
 
-    static categoryImports(language) {
-      let result = {}
-      let keys = Object.keys(categories[language])
-      keys.forEach(key => {
-        try{
-          result[key] =  data[key][language];
-        }catch(e){
-          console.error(e)
-        }
-        
-      });
-      return result;
-    };
-  
-    static categories() {
-      return categories;
-    };
-  
-    static getCategory(category, language) {
-      if (typeof this.categoryImports(language)[category] === 'undefined') {
-        return null;
-      }
-  
-      return this.categoryImports(language)[category];
-    }
+  static categories(language) {
+    return categories[language];
+  };
+
+  static getCategory(category, language) {
+    return data[language][category];
   }
+}
